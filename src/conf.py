b4ac0a175b13ca5def5db64ac2b7e577765000ba
Ramon Saraiva <ramonsaraiva@gmail.com>
2019-05-11 14:03:18 -0300

    Use environs so conf can load from .env file

--- src/conf.py
+++ src/conf.py
@@ -1,19 +1,26 @@
-import os
+from environs import Env
+from pathlib import Path
 
 
-JIRA_AUTH = {
-    'options': {
-        'server': os.environ['JIRA_AUTH_SERVER'],
-    },
-    'basic_auth': (os.environ['JIRA_AUTH_EMAIL'],
-                   os.environ['JIRA_AUTH_PASS']),
-}
+BASE_DIR = Path(__file__).parents[0]
 
-GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
-GITHUB_USERS_EXCLUDE = os.getenv('GITHUB_USERS_EXCLUDE').split(', ')
+env = Env()
+env.read_env(BASE_DIR)
 
+with env.prefixed('JIRA_AUTH_'):
+    JIRA_AUTH = {
+        'options': {
+            'server': env.str('SERVER'),
+        },
+        'basic_auth': (env.str('EMAIL'), env.str('PASS')),
+    }
 
-SLACK_WEBHOOK_URL = os.environ['SLACK_WEBHOOK_URL']
-SLACK_WEBHOOK_USERNAME =  os.environ['SLACK_WEBHOOK_USERNAME']
-SLACK_WEBHOOK_USERNAME_ICON_URL = os.environ['SLACK_WEBHOOK_USERNAME_ICON_URL']
-SLACK_NOTIFY_CHANNEL = os.environ['SLACK_NOTIFY_CHANNEL']
+with env.prefixed('GITHUB_'):
+    GITHUB_TOKEN = env.str('TOKEN')
+    GITHUB_USERS_EXCLUDE = env.str('USERS_EXCLUDE').split(', ')
+
+with env.prefixed('SLACK_'):
+    SLACK_WEBHOOK_URL = env.str('WEBHOOK_URL')
+    SLACK_WEBHOOK_USERNAME =  env.str('WEBHOOK_USERNAME')
+    SLACK_WEBHOOK_USERNAME_ICON_URL = env.str('WEBHOOK_USERNAME_ICON_URL')
+    SLACK_NOTIFY_CHANNEL = env.str('NOTIFY_CHANNEL')
