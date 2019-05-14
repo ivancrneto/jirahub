b015860448f73ccd03d48eb72ca5dfcdb257271a
Ramon Saraiva <ramonsaraiva@gmail.com>
2019-05-14 13:12:16 -0300

    Core docstrings

--- src/core.py
+++ src/core.py
@@ -30,14 +30,22 @@ class Issue:
 
     @property
     def key(self):
+        """Returns the JIRA issue key."""
         return self._data.key
 
     def transition_date(self, from_, to):
+        """
+        Tries fetching a specific transition in all histories of the
+        issue changelog, from a specific status, to a specific status.
+
+        '?' can be used as a wild card in case you want to fetch a transition
+        to something, not considering the `from` status, or vice-versa.
+        """
         for history in self._data.changelog.histories:
             for transition in history.items:
                 if (
-                    transition.fromString == from_
-                    and transition.toString == to
+                    (transition.fromString == from_ or from_ == '?')
+                    and (transition.toString == to or to == '?')
                 ):
                     return arrow.get(history.created)
         return None
@@ -50,6 +58,10 @@ class PullRequest:
         self._comments = None
 
     def get_comments(self):
+        """
+        Retrieves all comments from the PullRequest, this does an
+        additional API call to GitHub.
+        """
         if self._comments is not None:
             return self._comments
         self._comments = self._data.get_issue_comments()
@@ -57,6 +69,10 @@ class PullRequest:
 
     @property
     def has_human_comments(self):
+        """
+        Returns `True` when at least one of the PullRequest comments
+        was not written by a bot or integration.
+        """
         for comment in self._get_comments():
             if comment.user.login not in GITHUB_USERS_EXCLUDE:
                 return True
@@ -64,10 +80,12 @@ class PullRequest:
 
     @property
     def created_at(self):
+        """Returns an arrow instance of the PullRequest `created_at`"""
         return arrow.get(self._data.created_at)
 
     @property
     def review_started(self):
+        """Returns `True` if the a review was started in this PullRequest."""
         return (
             len(self._data.assignees)
             or self.has_human_comments
